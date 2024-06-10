2611472886e907001f7a94c092f493ea67457d91
Rupinder Singh <rupinder.developer@gmail.com>
2024-06-10 12:04:52 +0530

    Minor fix

--- global.d.ts
+++ global.d.ts
@@ -1,5 +1,4 @@
-// Requirements
-import ExceptionInterface from '@/core/interfaces/exception';
+import { ExceptionInterface } from '@/core/interfaces/exception';
 
 declare global {
   class Exception {
